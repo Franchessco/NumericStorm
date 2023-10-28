82ddb6e893900b7d850a6cb4df1ee6ef7bd35a0e
Franchessco <fzabier@gmail.com>
2023-10-28 17:13:00 +0200

    fixed std::uniq_ptr, and added alternativ way of declaring model, later add macro to automate this

--- NumericWorm/NumericStorm/headers/Fitting/Fitter.hpp
+++ NumericWorm/NumericStorm/headers/Fitting/Fitter.hpp
@@ -7,22 +7,30 @@
 namespace NumericStorm {
 
 typedef std::unique_ptr<Data<double>> (*func)(const Parameters& param);
+// typedef Data<double> (*func)(const Parameters& param);
 class Fitter
 {
 private:
 	Parameters m_proposalParameters;
 protected:
 	Parameters m_fittedParameters;
-	Data<double> *m_data ;
+	std::unique_ptr<Data<double>> m_data;
 	func m_model;
 	Fitter(func model, Data<double>& data,Parameters proposalParameters)
 		:m_model(model), m_data(&data),m_proposalParameters(proposalParameters){};
-
+	~Fitter() {};
 public:
 	virtual Parameters getParameters() const = 0;
 	virtual void setParameters(Parameters param) = 0;
 	virtual std::unique_ptr<Data<double>> model(Parameters param) const 
 		{ return m_model(param); };
+	/*
+	
+	virtual Data<double> model(Parameters param) const
+	{
+		return m_model(param);
+	};
+	*/
 	virtual Parameters fit(const Data<double>& dataToFit) = 0;
 };
 
