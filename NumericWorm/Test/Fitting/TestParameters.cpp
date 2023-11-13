e3dfd96314ab424900eb329a59f1580b9a269833
Franchessco <fzabier@gmail.com>
2023-11-13 18:59:20 +0100

    Finished Simple Test for Parameters

--- NumericWorm/Test/Fitting/TestParameters.cpp
+++ NumericWorm/Test/Fitting/TestParameters.cpp
@@ -9,17 +9,14 @@ struct TestParameters:public testing::Test
 public:
 
     // Test data
-    std::array<double, 2> d1{ 1, 2.2 };
-    std::array<int, 2> d2{ 1, 2 };
-    std::array<std::string, 3> d3{ "Parameters", "Testing", "something" };
+
     Parameters<int, 2> p1{ 1, 2 };
     Parameters<double, 2> p2{ 1.0, 2.2 };
     Parameters<std::string, 3> p3{ "Parameters", "Testing", "something" };
 
-    Parameters<double, 2> p4{ d1 };
-    Parameters<int, 2> p5{ d2 };
-    Parameters<std::string, 3> p6{ d3 };
-
+    std::array<int, 2> d1{ 1, 2 };
+    std::array<double, 2> d2{ 1.0, 2.2 };
+    std::array<std::string, 3> d3{ "Parameters", "Testing", "something" };
 };
 
 
@@ -28,23 +25,59 @@ TEST_F(TestParameters, constructingParametersbylist)
 {
     auto a = p1.getParamters();
     EXPECT_EQ(a[0], 1);
+    EXPECT_EQ(a[1], 2);
+
     std::array<int, 2> a1{ 1,2 };
-    std::array<int, 2> a2{ 1,2 };
-    bool b1 = (a == a1);
+    std::array<int, 2> a2{ 1,3 };
+    bool b1 = (a == a1),b2= (a==a2);
     
     EXPECT_EQ(b1,1);
+    EXPECT_EQ(b2, 0);
 
 }
-//TEST_F(TestParameters, constructingParametersbyarray) 
-//{
-//
-//}
+TEST_F(TestParameters, constructingParametersbyarray) 
+{
 
 
-//TEST(PARAMETERS, indexOperator) 
-//{
-//	Parameters<double, 2> p(1, 2);
-//	EXPECT_EQ(p[0],1);
-//	FAIL(p[2]);
-//
-//}
+    Parameters<int, 2> p4{ d1 };
+    Parameters<double, 2> p5{ d2 };
+    Parameters<std::string, 3> p6{ d3 };
+
+    bool b1 = (p4.getParamters() == d1), b2 = (p5.getParamters() == d2), b3 = (p6.getParamters() == d3);
+
+    EXPECT_EQ(b1,1);
+    EXPECT_EQ(b2,1);
+    EXPECT_EQ(b3,1);
+
+}
+TEST_F(TestParameters, operatorequal) 
+{
+
+    EXPECT_EQ(p1, d1);
+    EXPECT_EQ(p2, d2);
+    EXPECT_EQ(p3, d3);
+
+}
+TEST_F(TestParameters, gettingParameters) 
+{
+    int a1;double a2;std::string a3;
+
+    a1 = p1[1];a2 = p2[1];a3 = p3[1];
+    int t1 = d1[1]; double t2 = d2[1]; std::string t3 = d3[1];
+
+    EXPECT_EQ(a1,t1);
+    EXPECT_EQ(a2,t2);
+    EXPECT_EQ(a3,t3);
+
+}
+
+TEST_F(TestParameters, settingParameters) 
+{
+    int t1 = 5; double t2 = 15.8; std::string t3 = "Cherno";
+
+    p1[1] = t1; p2[1] = t2; p3[1] = t3;
+    EXPECT_EQ(p1[1],t1);
+    EXPECT_EQ(p2[1], t2);
+    EXPECT_EQ(p3[1], t3);
+
+}
